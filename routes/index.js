70cde57c1df6bf42bf35c1d39eb0137dd8e1e97e
Kai Ho CHAN <Crashes.John@gmail.com>
2023-03-30 16:33:05 +0800

    Backend prototype code added
    Tested, GET and POST method give intended results.

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var hr = 0;
+var rr = 0;
+
+/* GET HR and RR JSON object */
+router.get('/', function(req, res, next) {
+  res.json({hr, rr}).end();
+});
+
+/* POST update HR and RR */
+router.post('/', function(req, res) {
+  hr = req.body.hr;
+  rr = req.body.rr;
+  res.end();
+})
+
+module.exports = router;

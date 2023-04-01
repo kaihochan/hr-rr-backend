8506160071fc0505c87b9a6de7cc5c4540758886
Kai Ho CHAN <Crashes.John@gmail.com>
2023-04-01 22:48:33 +0800

    Added POST method for updating the plot

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var fs = require('fs');
+const path = require('path');
 var router = express.Router();
 var hr = 0;
 var rr = 0;
@@ -13,6 +15,13 @@ router.post('/', function(req, res) {
   hr = req.body.hr;
   rr = req.body.rr;
   res.end();
+});
+
+/* POST update plot */
+router.post('/plot/:name', function(req, res) {
+  fs.writeFile(path.join(__dirname, `../public/image/${req.params.name}.png`), req.body.image, 'base64', (err) => {
+    err ? res.sendStatus(500) : res.end();
+  });
 });
 
 module.exports = router;
